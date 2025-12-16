4d57999eea905b8a25368d917f306bb1f906b40e
JasonZhang1909 <jsh27335@gmail.com>
2025-12-16 00:55:03 -0600

    everything up to date, removed phanton nul

--- market-maker/lib/realtime/server.ts
+++ market-maker/lib/realtime/server.ts
@@ -1,6 +1,7 @@
-import {Server} from "socket.io"
+import { Server as HttpServer } from "http";
+import { Server } from "socket.io";
 
-export function createSocketServer(httpServer:any){
+export function createSocketServer(httpServer: HttpServer) {
     return new Server(httpServer, {
         cors:{origin:"*"},
     });
