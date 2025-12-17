529869c3edb95e7fe06ff2a804c6e9aba58a6728
JasonZhang1909 <jsh27335@gmail.com>
2025-12-16 21:11:25 -0600

    completed users, question, game tables and established their relation

--- market-maker/lib/schema/schema.ts
+++ market-maker/lib/schema/schema.ts
@@ -10,3 +10,12 @@ import {
 } from "drizzle-orm/pg-core";
 
 // Define your tables here
+
+export const users = pgTable('users', {
+  id: uuid("id").primaryKey().defaultRandom(),
+  clerkUserId: text("clerk_user_id").notNull().unique(),
+  displayName: text("display_name"),
+  createdAt: timestamp("created_at",{withTimezone: true}).notNull().defaultNow(),
+}
+
+)
